9e1fe09d214b73b2b1c290c9566ef718a87efafc
basyura <basyura@gmail.com>
2011-03-07 11:42:57 +0900

    set table name when extended

--- active_axs/base.rb
+++ active_axs/base.rb
@@ -1,7 +1,54 @@
 
 module ActiveAXS
+  def self.Base(table_name)
+    c = Class.new(ActiveAXS::Base)
+    sig = class << c ; self end
+    sig.send :define_method , :table_name , Proc.new{table_name}
+    c
+  end
   class Base < ActiveAXS::Core
     include Enumerable
+    def self.inherited(subclass)
+      super
+
+      return if subclass.superclass == ActiveAXS::Base
+
+      table = subclass.table_name
+      if table.index("\.")
+        table = table_name.split("\.")[1]
+      end
+
+      list = []
+      schema = create_connection.OpenSchema(4 , [nil, nil, table])
+      schema.extend ActiveAXS::RecordSet
+      schema.each_record{|rec|
+        list << [rec["Column_Name"] , rec["Data_Type"]]
+      }
+      #sig = class << subclass ; self end
+      #sig.send :define_method , :get_schema , Proc.new{list}
+      subclass.send :define_method , :get_schema , Proc.new{list}
+
+      #set.each_record{|r|
+        #table_name = r["Table_Name"]
+        #schemas = connection.OpenSchema(4 , [nil, nil, table_name])
+        #schemas.extend ActiveAXS::RecordSet
+        #puts table_name
+        #schemas.each_record{|r2|
+          #puts r2["Column_Name"]
+        #}
+      #}
+      # get fields
+      #schemas = connection.OpenSchema(4 , [nil, nil, table_name])
+      #schemas.extend ActiveAXS::RecordSet
+      #schemas.each_record{|r2|
+      #  puts r2["Column_Name"] + "-" + r2["Data_Type"].to_s
+      #}
+      #primary = connection.OpenSchema(28 , ["pubs", "dbo", table_name])
+      #primary.extend ActiveAXS::RecordSet
+      #primary.each_record{|r2|
+      #  puts r2["Column_Name"]
+      #}
+    end
     def initialize(map={})
       @__stored_map__ = {}
       map.each_pair do |key , value|
@@ -9,8 +56,10 @@ module ActiveAXS
       end
     end
     def save
-      keys = @__stored_map__.keys
 
+      raise StandardError.new('not suppoted') if @__stored_map__.empty?
+
+      keys = @__stored_map__.keys
       sql = "insert into #{self.class.table_name} (#{keys.join(',')}) values( "
 
       keys.each_with_index do |key , index|

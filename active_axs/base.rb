20ff5512237bfd369c2ccaa2aa7c6af4865886be
basyura <basyura@gmail.com>
2011-03-10 09:38:57 +0900

    refactored

--- active_axs/base.rb
+++ active_axs/base.rb
@@ -1,14 +1,20 @@
 
 module ActiveAXS
+
+  # new ActiveAXS::Base with table name
   def self.Base(table_name)
     c = Class.new(ActiveAXS::Base)
     sig = class << c ; self end
     sig.send :define_method , :table_name , Proc.new{table_name}
     c
   end
+
+  ##
+  # ActiveAXS::Base
   class Base < ActiveAXS::Core
     include Enumerable
 
+    # inherited
     def self.inherited(subclass)
       super
 
@@ -23,46 +29,27 @@ module ActiveAXS
       schema = create_connection.OpenSchema(4 , [nil, nil, table])
       schema.extend ActiveAXS::RecordSet
       schema.each_record{|rec|
-        list << [rec["Column_Name"] , rec["Data_Type"]]
+        list << [rec["Column_Name"].downcase.to_sym , rec["Data_Type"]]
       }
-      #sig = class << subclass ; self end
-      #sig.send :define_method , :get_schema , Proc.new{list}
       subclass.send :define_method , :get_schema , Proc.new{list}
-
-      #set.each_record{|r|
-        #table_name = r["Table_Name"]
-        #schemas = connection.OpenSchema(4 , [nil, nil, table_name])
-        #schemas.extend ActiveAXS::RecordSet
-        #puts table_name
-        #schemas.each_record{|r2|
-          #puts r2["Column_Name"]
-        #}
-      #}
-      # get fields
-      #schemas = connection.OpenSchema(4 , [nil, nil, table_name])
-      #schemas.extend ActiveAXS::RecordSet
-      #schemas.each_record{|r2|
-      #  puts r2["Column_Name"] + "-" + r2["Data_Type"].to_s
-      #}
-      #primary = connection.OpenSchema(28 , ["pubs", "dbo", table_name])
-      #primary.extend ActiveAXS::RecordSet
-      #primary.each_record{|r2|
-      #  puts r2["Column_Name"]
-      #}
     end
+
+    # initialize
+    # new ActiveAXS::Base with record map
+    # store record to instance variable
     def initialize(map)
       @__stored_map__ = {}
       map.each_pair do |key , value|
         @__stored_map__[key.downcase.to_sym] = value
       end
     end
-    def ondb?
-      @ondb
-    end
+
+    # save record. 
+    # now this method is insert only
     def save
-
-      #raise StandardError.new('not suppoted') if @__stored_map__.empty?
-
+      # raise error if schema is not same between from and to.
+      # so , create sql from record map
+      #keys = get_schema.map{|v| v[0]}
       keys = @__stored_map__.keys
       sql = "insert into #{self.class.table_name} (#{keys.join(',')}) values( "
 
@@ -74,20 +61,31 @@ module ActiveAXS
       sql << ")"
       self.class.execute_sql(sql)
     end
+
+    # delete record with where keys
+    # throw ArgumentError if where is empty
     def self.delete(where = {})
       raise ArgumentError if where.empty?
       execute_delete(where)
     end
+
+    # find first record
     def self.find_first(keys)
       execute_select(keys).first
     end
+
+    # find all record
     def self.find_all(keys={})
       execute_select(keys)
     end
+
+    # find record with sql
     def self.find_by_sql(sql)
       raise Exception.new("not select sql") if sql.strip !~ /^select/i
       execute_sql(sql)
     end
+
+    # accessor for record map
     def method_missing(method , *args)
       if method =~ /.*=/ && args.length == 1
         @__stored_map__[method.to_s.sub("=","").to_sym] = args[0]
--- active_axs/core.rb
+++ active_axs/core.rb
@@ -1,35 +1,28 @@
 
 module ActiveAXS
   class Core
+    
+    # get table name
     def self.table_name
       self.to_s + "s"
     end
-    def self.set_source(source)
-      define_attr_method :connect_source , source
-    end
-    def self.set_table_name(table_name)
-      define_attr_method :table_name , table_name
-    end
-    def self.set_user_id(user_id)
-      define_attr_method :connect_user_id , user_id
-    end
-    def self.set_password(password)
-      define_attr_method :connect_password , password
-    end
+
+    # establish connection
     def self.establish_connection(config)
-      set_source(config[:source]) if config.has_key?(:source)
-      set_user_id(config[:user_id]) if config.has_key?(:user_id)
-      set_password(config[:password]) if config.has_key?(:password)
+      define_attr_method :connect_source   , config[:source]   if config.has_key?(:source)
+      define_attr_method :connect_user_id  , config[:user_id]  if config.has_key?(:user_id)
+      define_attr_method :connect_password , config[:password] if config.has_key?(:password)
       create_connection.close
     end
-    #
+
     # private method
-    #
     private
     def self.define_attr_method(name, value)
       sing = class << self; self; end
       sing.class_eval "def #{name}; #{value.to_s.inspect}; end" 
     end
+
+    # create connection
     def self.create_connection
       connection = WIN32OLE.new("ADODB.Connection").extend Connection
       connstr = ""
@@ -40,42 +33,21 @@ module ActiveAXS
         connstr << "DSN=#{connect_source};User ID=#{connect_user_id};Password=#{connect_password};"
       end
       connection.open(connstr)
-
       
-      # I want to get primary key. but I can't.
-      #
-      #set = connection.OpenSchema(20)
-      #set.extend ActiveAXS::RecordSet
-      #
-      #set.each_record{|r|
-      #  table_name = r["Table_Name"]
-      #  schemas = connection.OpenSchema(4 , [nil, nil, table_name])
-      #  schemas.extend ActiveAXS::RecordSet
-      #  puts table_name
-      #  schemas.each_record{|r2|
-      #    puts r2["Column_Name"]
-      #  }
-      #}
-      # get fields
-      #schemas = connection.OpenSchema(4 , [nil, nil, table_name])
-      #schemas.extend ActiveAXS::RecordSet
-      #schemas.each_record{|r2|
-      #  puts r2["Column_Name"] + "-" + r2["Data_Type"].to_s
-      #}
-      #primary = connection.OpenSchema(28 , ["pubs", "dbo", table_name])
-      #primary.extend ActiveAXS::RecordSet
-      #primary.each_record{|r2|
-      #  puts r2["Column_Name"]
-      #}
-
       return connection
     end
+
+    # execute select sql
     def self.execute_select(keys)
       execute_sql(ActiveAXS::SQLGenerator.create_select_sql(table_name , keys))
     end
+
+    # execute delete sql
     def self.execute_delete(where)
       execute_sql(ActiveAXS::SQLGenerator.create_delete_sql(table_name , where))
     end
+
+    # execute sql
     def self.execute_sql(sql)
       begin 
         connection = create_connection
@@ -95,6 +67,8 @@ module ActiveAXS
       end
     end
   end
+
+  # Connection
   module Connection
     def open(constr)
       self.Open constr
